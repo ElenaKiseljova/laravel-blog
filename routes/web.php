3c7f65d519c7bd62b392cbbab38994dee0bdfaa1
Elena Kiseljova <e.a.kiseljova@gmail.com>
2025-03-19 18:24:00 +0200

    add: index and show routes

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('posts.index');
+})->name('posts.index');
+
+Route::get('/posts/{post}', function ($post) {
+    return view('posts.show', compact('post'));
+})->name('posts.show');
